a42bff1145fd22c80eb68b13a5f1c0a24234848e
Paridhi Singla <160217776+paridhisingla@users.noreply.github.com>
2026-09-25 20:16:53 +0530

    Time: 1 ms (99.79%), Space: 47.3 MB (38.8%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,21 +3,18 @@ class Solution {
         int n=nums.length;
         int ans=0;
         int end=0;
-
-
-        int farthest=0;
-    for(int i=0; i<n-1; i++){
-        farthest=Math.max(farthest, nums[i]+i);
-        if(farthest>=n-1){
-            ans++;
-            return ans;
-        }
-        if(i==end){
-            ans++;
-            end=farthest;
-        }
-    }
-    return ans;
-        
+         int farthest=0;
+         for(int i=0; i<n-1; i++){
+            farthest=Math.max(farthest, nums[i]+i);
+            if(farthest>=n-1){
+                ans++;
+                return ans;
+            }
+            if(i==end){
+                ans++;
+                end=farthest;
+            }
+         }
+         return ans;
     }
 }
