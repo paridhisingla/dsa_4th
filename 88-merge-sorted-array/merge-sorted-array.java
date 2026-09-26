fb1b3871505d877ab8ab8de677b0fe9b15ceb885
Paridhi Singla <160217776+paridhisingla@users.noreply.github.com>
2026-09-26 14:39:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (46.89%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -5,10 +5,9 @@ class Solution {
         int k=m+n-1;
         while(j>=0){
             if(i>=0 && nums1[i]> nums2[j]){
-              nums1[k]=nums1[i];
-              k--;
-              i--;
-
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
             else{
                 nums1[k]=nums2[j];
