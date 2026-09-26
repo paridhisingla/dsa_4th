0726696623c269fa521fac3dd1b0fd0b85ee4121
Paridhi Singla <160217776+paridhisingla@users.noreply.github.com>
2026-09-26 15:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (83.17%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int[] bucket= new int[n+1];
+        for(int i=0; i<n; i++){
+            int x= citations[i];
+            if(x>n) bucket[n]++;
+            else bucket[x]++;
+        }
+        int sum=0;
+        for(int i=n; i>0; i--){
+            sum+=bucket[i];
+            if(sum>=i) return i;
+        }
+         return 0;
+    }
+}
